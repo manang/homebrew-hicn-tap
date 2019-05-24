0c873adc09c26b29524dd3a857c7369a1d1e69d7
Angelo Mantellini <manangel@cisco.com>
2019-05-24 13:24:48 +0200

    fix

--- hicn.rb
+++ hicn.rb
@@ -1,6 +1,6 @@
 class Hicn < Formula
   desc "Hicn stack"
-  homepage ""
+  homepage "https://wiki.fd.io/view/HICN"
   url "https://github.com/FDio/hicn/archive/master.zip"
   version "1.0"
   sha256 ""
@@ -9,22 +9,11 @@ class Hicn < Formula
   depends_on "curl"
   depends_on "asio"
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "ls -la"
     system "cmake", ".", "-DBUILD_APPS=ON",*std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install" , "-j"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libparc`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
--- libparc.rb
+++ libparc.rb
@@ -9,21 +9,11 @@ class Libparc < Formula
   depends_on "libevent"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     system "cmake", "libparc", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install", "-j"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test libparc`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end

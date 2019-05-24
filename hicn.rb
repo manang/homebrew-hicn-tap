0b0baffd5c458398dbb9b7728d2cac9e416b6051
Angelo Mantellini <manangel@cisco.com>
2019-05-24 14:10:49 +0200

    remove hicn 19.04 formula

--- hicn.rb
+++ hicn.rb
@@ -1,19 +1,17 @@
-class Hicn < Formula
+class HicnMaster < Formula
   desc "Hicn stack"
   homepage "https://wiki.fd.io/view/HICN"
-  url "https://github.com/FDio/hicn/archive/v19.04.zip"
+  url "https://github.com/FDio/hicn/archive/master.zip"
   version "19.04"
   sha256 ""
   depends_on "cmake" => :build
   depends_on "libparc"
   depends_on "curl"
   depends_on "asio"
-  conflicts_with "hicn-master", :because => "the name of the generated binaries are the same"
   def install
     system "cmake", ".", "-DBUILD_APPS=ON",*std_cmake_args
     system "make", "install" , "-j"
   end
-
   test do
     system "false"
   end
